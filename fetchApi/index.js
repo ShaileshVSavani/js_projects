b4375c501ae1cea22559d7efe0897c737df728f9
ShaileshVSavani <svs.rnw23@gmail.com>
2024-03-21 14:55:53 +0530

    1

--- fetchApi/index.js
+++ fetchApi/index.js
@@ -1,7 +1,7 @@
-const ui = (data) => { 
+const ui = (data) => {
     console.log(data);
     
-    data.map((ele) => { 
+    data.map((ele) => {
         
         let brand = document.createElement("h2");
         brand.innerHTML = `Brand: ${ele.brand}`;
@@ -19,14 +19,14 @@ const ui = (data) => {
         id.innerHTML = `ID: ${ele.id}`;
 
         let imagesContainer = document.createElement("div");
-        imagesContainer.setAttribute( "class", "images-container");
+        imagesContainer.setAttribute("class", "images-container");
     
-            ele.images.map((imgUrl) => {
-                let images = document.createElement("img");
-                images.src = imgUrl;
-                images.setAttribute("class","product-img");
-                imagesContainer.append(images);
-            });
+        ele.images.map((imgUrl) => {
+            let images = document.createElement("img");
+            images.src = imgUrl;
+            images.setAttribute("class", "product-img");
+            imagesContainer.append(images);
+        });
       
 
         let price = document.createElement("h3");
@@ -48,19 +48,50 @@ const ui = (data) => {
         
         let div = document.createElement("div");
         // div.classList.add("product");
-        div.setAttribute( "class" ,"product");
+        div.setAttribute("class", "product");
        
         div.append(
             id, title, brand, category, description, discountPercentage,
-             price, rating, stock, imagesContainer
+            price, rating, stock,  imagesContainer
         );
 
     
         document.getElementById("box").append(div);
     });
-}
+   
+}  
+    fetch('https://dummyjson.com/products')
+        .then(response => response.json())
+        .then(data => ui(data.products))
 
-fetch('https://dummyjson.com/products')
-    .then(response => response.json())
-    .then(data => ui(data.products))
 
+
+
+//-----------------------------------------------------------------
+// const ui = (data) => {
+//     const items = data.map((item) => `
+//                  <div class="col-lg-4 col-md-6 col-sm-12">
+//                      <div class="product">
+//                         <h3>ID: ${item.id}</h3>
+//                         <h2>Title: ${item.title}</h2>
+//                         <h2>Brand: ${item.brand}</h2>
+//                         <h3>Category: ${item.category}</h3>
+//                         <p>Description: ${item.description}</p>
+//                         <h3>Discount Percentage: ${item.discountPercentage}%</h3>
+//                         <h3>Price: Rs. ${item.price}</h3>
+//                         <h3>Rating: ${item.rating}/5</h3>
+//                         <h3>Stocks: ${item.stock}</h3>
+//                         <img src="${item.thumbnail}" alt="Thumbnail" class="product-thumbnail">
+//                     </div>
+//                 </div>
+//     `).join(''); /*We use .map() to iterate over each item in the data array, and for each item, we generate the corresponding HTML string.
+//     We use join('') to concatenate all the HTML strings into a single string, which will be assigned to the items variable.*/
+
+//     document.getElementById("box").innerHTML = items;
+// };
+
+// fetch('https://dummyjson.com/products')
+//     .then(response => response.json())
+//     .then(data => ui(data.products));
+
+ 
